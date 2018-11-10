1d0ff9b52f9f8b6ba26183a4aee7d94d785813cf
Chris <chriswebb2309@gmail.com>
2018-11-10 22:27:27 +0000

    added a shuffle deck function

--- deck.go
+++ deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 // CreateDeck creates an unshuffled deck of cards
 func CreateDeck() []string {
@@ -19,10 +23,26 @@ func CreateDeck() []string {
 	return cards
 }
 
-//DealCard takes a deck of cards and returns the top card and a new deack with the top card removed
+//DealCard takes a deck of cards and returns the top card and a new deck with the top card removed
 func DealCard(d []string) (string, []string) {
 	dealt := d[0]
 	remaining := d[1:len(d)]
 
 	return dealt, remaining
+}
+
+// ShuffleDeck takes a deck and shuffles it
+func ShuffleDeck(unshuffled []string) []string {
+
+	randomizer := rand.New(rand.NewSource(time.Now().Unix()))
+	noCards := len(unshuffled)
+	shuffled := make([]string, noCards)
+
+	for i := 0; i < noCards; i++ {
+		randIndex := randomizer.Intn(len(unshuffled))
+		shuffled[i] = unshuffled[randIndex]
+		unshuffled = append(unshuffled[:randIndex], unshuffled[randIndex+1:]...)
+	}
+
+	return shuffled
 }
--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 
-	fmt.Println("Hello")
+	myDeck := CreateDeck()
+	fmt.Println("Unshuffled")
+	fmt.Println(strings.Join(myDeck, ", "))
+
+	myDeck = ShuffleDeck(myDeck)
+	fmt.Println("Shuffled")
+	fmt.Println(strings.Join(myDeck, ", "))
 
 }
